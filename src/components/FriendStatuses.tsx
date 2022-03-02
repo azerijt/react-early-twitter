e9218cff7dc4d9e42d997ece8b962ca0ba0de642
Anna-Zeri Temir <azjuliantemir@gmail.com>
2022-03-02 16:29:44 +0000

    completed most of exercises

--- src/components/FriendStatuses.tsx
+++ src/components/FriendStatuses.tsx
@@ -29,25 +29,41 @@ function FriendStatuses(): JSX.Element {
           emoji={"◻️"}
           timeAgo={{ n: 19, unit: "minutes" }}
         />
+           <StatusItem
+          username={"drx"}
+          content={"chatting in gmail w/Jack"}
+          emoji={"⭐"}
+          timeAgo={{ n: 31, unit: "minutes" }}
+        />
+        <StatusItem
+          username={"biz"}
+          content={"having some coffee"}
+          emoji={"◻️"}
+          timeAgo={{ n: 36, unit: "minutes" }}
+        />
+        <StatusItem
+          username={"Florian"}
+          content={"Preparing a pizza"}
+          emoji={"◻️"}
+          timeAgo={{ n: 3, unit: "hours" }}
+        />
+        
+        <StatusItem
+          username={"Courtney"}
+          content={"multi-tasking audio debug audio coding"}
+          emoji={"◻️"}
+          timeAgo={{ n: 8, unit: "hours" }}
+        />
+      
+        <StatusItem
+          username={"Jack"}
+          content={"fantasizing about jack drawing naked people mmmmmmmmm... naked people."}
+          emoji={"◻️"}
+          timeAgo={{ n: 21, unit: "hours" }}
+        />
+     
         <li>
-          ⭐ <a href="#">drx</a>: chatting in gmail w/Jack
-          <small>31 minutes ago</small>
-        </li>
-        <li>
-          ⭐ <a href="#">biz</a>: having some coffee
-          <small>36 minutes ago</small>
-        </li>
-        <li>
-          ⭐ <a href="#">Florian</a>: Preparing a pizza
-          <small>about 3 hours ago</small>
-        </li>
-        <li>
-          ⭐ <a href="#">Courtney</a>: multi-tasking audio debug audio coding
-          <small>about 8 hours ago</small>
-        </li>
-        <li>
-          ◻️ <a href="#">Jack</a>: fantasizing about jack drawing naked people
-          mmmmmmmmmmmmm..... naked people. <small>about 21 hours ago</small>
+          ◻️ <a href="#">Jack</a>:  <small>about 21 hours ago</small>
         </li>
         <li>
           ⭐ <a href="#">noah</a>: Oh crap, I think I might be getting that f'in
